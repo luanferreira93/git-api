3ed253558a85dee5894581f3b559856b748c77d8
Luan Carlos <65790853+luanferreira93@users.noreply.github.com>
2022-02-10 02:57:14 -0300

    Update main.js

--- assests/js/main.js
+++ assests/js/main.js
@@ -1,7 +1,6 @@
 const avatar = document.querySelector('#avatar_img');
 const id = document.querySelector('#id');
 const name = document.querySelector('#name');
-const url = document.querySelector('#url');
 const repos = document.querySelector('#repos');
 const follos = document.querySelector('#followers');
 const foolow = document.querySelector('#following');
@@ -12,7 +11,6 @@ axios.get(api_url).then(res=>{
         avatar.setAttribute('src',res.data.avatar_url);
         createLine(name,res.data.name);
         createLine(id,res.data.login);
-        createLine(url,res.data.url);
         createLine(repos,res.data.public_repos);
         createLine(follos,res.data.followers);
         createLine(foolow,res.data.following);
